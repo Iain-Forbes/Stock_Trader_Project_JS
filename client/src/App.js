092421d27ceee930ecbce7e17aa8b58e7653c68d
Kirsten Nicholson <kirstennic6@gmail.com>
2021-01-09 23:35:26 +0000

    added updateStock and created StockList.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {useState, useEffect} from "react";
 import {getStocks} from "./StockService";
-
+import StockList from "./StockList";
 
 
 function App() {
@@ -15,17 +15,38 @@ function App() {
   }, [])
 
   const addStock = (stock) => {
-    const buyStock = stock.map(s => s);
+    const buyStock = stock.map(shares => shares);
     buyStock.push(stock);
     setStocks(buyStock);
   }
 
+  const updateStock = updatedStock => {
+    //update in DB 
+    updateStock(updatedStock);
+    //update locally
+    const updatedStocksIndex = stocks.findIndex(stock => stock._id === updatedStock._id);
+    const updatedStocks = [...stocks];
+    updatedStocks[updatedStocksIndex] = updatedStocks;
+    setStocks(updatedStocks);
+  };
+
   const deleteStock = (id) => {
-    const removeStock = 
+    const removeStock = stocks.map(shares => shares);
+    const stockToDelete = removeStock.map(shares => shares._id).indexOf(id);
+    console.log(stockToDelete);
+
+    removeStock.splice(stockToDelete, 1);
+    setStocks(removeStock);
+
   }
+
   return (
     <>
     <h1> Making millions from stocks and shares!</h1>
+    <StockList
+    updateStock = {updateStock}
+    deleteStock = {deleteStock}
+    />
     </>
   );
 }
