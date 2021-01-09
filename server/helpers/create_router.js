3c718afeb7a572d0a9bbb49dd8afd43d1bb6c4f8
Kirsten Nicholson <kirstennic6@gmail.com>
2021-01-09 13:49:36 +0000

    added get one route

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const ObjectID = require('mongodb').ObjectID;
+
 const createRouter = function (collection) {
 
   const router = express.Router();
@@ -9,6 +10,18 @@ const createRouter = function (collection) {
       .find()
       .toArray()
       .then((docs) => res.json(docs))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+    });
+
+    router.get('/:id', (req, res) => {
+      const id = req.params.id;
+      collection
+      .findOne({_id: ObjectID(id)})
+      .then((doc) => res.json(doc))
       .catch((err) => {
         console.error(err);
         res.status(500);
