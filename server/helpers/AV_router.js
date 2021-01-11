43e65feee4f7ccf5ea41413d0c4d141a7f527b69
Iain Forbes <i.forbes77@gmail.com>
2021-01-11 07:49:17 +0000

    Added missing import

--- server/helpers/AV_router.js
+++ server/helpers/AV_router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
+const app = express();
 
 const cors = require("cors");
 app.use(cors());
