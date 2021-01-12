8fb6407f981ac9893b21c2303f4f8ea9acdae60a
Iain Forbes <i.forbes77@gmail.com>
2021-01-12 14:50:17 +0000

    acontainers

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,13 @@
 import {useState, useEffect} from "react";
 import {getPortfolio} from "./Services/PortfolioService";
-import {getStockIndex, getStockSymbol} from "./Services/StockService";
-import StockList from "./Portfolio/PortfolioList";
+import StockList from "./StockData/StockList";
 import SearchForm from "./SearchForm";
 import './App.css';
+import StockContainer from "./Containers/StockContainer"
 
 
 function App() {
   const [stocks, setPortfolio] = useState([]);
-  const [stockIndex, setStockIndex] = useState([]);
-  const [symbol, setsymbol] = useState([]);
 
   useEffect(() => {
     getPortfolio()
@@ -17,43 +15,6 @@ function App() {
       setPortfolio(allStocks);
     })  
   }, [])
-
-  useEffect(() => {
-    getStockIndex("ftse")
-    .then((allStockIndex) => {
-
-    const stockNodes = allStockIndex.feed.entry.map((stock) => {  
-      let output = {};
-        if (stock){
-          stock.content['$t'].split(", ").forEach(str => {
-            let [key, value] = str.split(": ");
-            if (value === "#N/A") {
-              value = null;
-          }
-            output[key] = value;
-          
-        });        
-      };   
-      return (
-        {title: stock.title['$t'],
-        name: output.name,
-        price: output.price,
-        change: output.change}
-      )  
-    });
-  })
-}, [])
-
-
-  useEffect(() => {
-    getStockSymbol("IBM")
-    .then((allSymbolData) => {
-      const values = Object.values(allSymbolData["Time Series (Daily)"])
-      console.log(values[0]["2. high"])
-    });
-    
-  }, [])
-
 
   const addStock = (stock) => {
     const buyStock = stock.map(shares => shares);
@@ -96,12 +57,12 @@ function App() {
     <div>
     <h1> Making millions from stocks and shares!</h1>
     <SearchForm searchStock={findStocks} />
-    <StockList
+    <StockContainer/>
+    {/* <StockList
     stocks={stocks}
     updateStock = {updateStock}
-    deleteStock = {deleteStock}
-    stockIndex = {stockIndex}
-    />
+    deleteStock = {deleteStock} */}
+    
     
     </div>
   );
---
+++ client/src/StockData/StockList.js
@@ -0,0 +1,9 @@
+const StockList = ({stockIndex}) => {
+    return(
+        <>
+        <p>Hello From Stock List</p>
+        </>
+        )
+}
+
+export default StockList;
