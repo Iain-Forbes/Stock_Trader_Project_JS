8dc555b96f613d552f7334c5d35d0ce247807f00
Kirsten Nicholson <kirstennic6@gmail.com>
2021-01-13 13:08:39 +0000

    StockForm

--- client/src/StockData/StockForm.js
+++ client/src/StockData/StockForm.js
@@ -9,22 +9,20 @@ const StockForm = (symbol, name, price) =>{
             <input type="text" id="symbol" name="symbol" value={symbol}/>
 
             <label for="name">Name</label>
-            <input type="text" id="name" name="name" />
+            <input type="text" id="name" name="name" value={name} />
 
             <label for="price">Price</label>
-            <input type="text" id="price" name="price" />
+            <input type="text" id="price" name="price" value={price}/>
 
             <label for="volume">Volume</label>
-            <input type="number" id="volume" name="volume" />
+            <input type="number" id="volume" name="volume" value="0" />
 
             <input
                 type="submit"
                 value="submit"
             />
 
-
         </form>
-    )
-
-}
+    )};
+    
 export default StockForm; 
