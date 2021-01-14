b6b1f9ad9378ef410e0eb21cdb0162e36c3a246c
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-01-14 17:51:32 +0000

    extracted xAxis and yAxis data from historical data

--- client/src/StockData/StockList.js
+++ client/src/StockData/StockList.js
@@ -6,6 +6,13 @@ const StockList = ({onStockSelected}) => {
 
     const [stockIndex, setStockIndex] = useState([]);
     const [symbol, setsymbol] = useState([]);
+    
+    // const [xAxis, setxAxis] = useState([]);
+    // const [yAxis, setyAxis] = useState([]);
+
+    let yAxis = [];
+    let xAxis = [];
+
   
     useEffect(() => {
       getStockIndex("ftse")
@@ -37,15 +44,38 @@ const StockList = ({onStockSelected}) => {
   }, [])
    
     useEffect(() => {
+
+      let count = 0;
+
       getStockSymbol("MSFT")
       .then((allSymbolData) => {
         const values = Object.values(allSymbolData["Time Series (Daily)"]).forEach(function (date) {
+
+          // Create x-axis data - using a count increment
+          xAxis.push(count);
+
+          // Create y-axis data - using closing position
+          yAxis.push(date["4. close"]);
+
+          // Increment
+          count += 1;
+
+
           Object.entries(date).forEach(function([...date]) {
-            console.log(date);
+            // console.log(date);
+
+            // I'm not sure that we need this part?
+            // I'm not sure that we need this part?
+            // I'm not sure that we need this part?
+
           })
         })
   
       });
+
+      // This most likely will be called BEFORE the data has been
+      // retrieved from the fetch, and therefore will be emmpty.
+      console.log("This is the ARRAY: " + yAxis);
 
     }, [])
     
