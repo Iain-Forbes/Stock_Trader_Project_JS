3760c2e833cf9811fb19aa5c236d9b5684edcf8c
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-01-14 16:56:12 +0000

    corrected Portfolio total

--- client/src/Portfolio/PortfolioList.js
+++ client/src/Portfolio/PortfolioList.js
@@ -4,8 +4,35 @@ import Portfolio from './Portfolio.js';
 const PortfolioList = ({stocks}) => {
     let portfolioTotal = 0
 
+    // function to check if string is a float
+    const isFloat = (x) => { return !!(x % 1); }
+
+    // function to check if the string is an int
+    const isInt = (value) => {
+        return !isNaN(value) && 
+               parseInt(Number(value)) == value && 
+               !isNaN(parseInt(value, 10));
+      }
+
     const portfolioInfo = stocks.map(stock => {
-        portfolioTotal += stock.price;
+
+
+
+
+        // convert price to numeric
+        // const price = parseFloat(stock.price);
+
+        if (isFloat(parseFloat(stock.price))) {
+            
+            // Accumulated total - using float data
+            portfolioTotal += parseFloat(stock.price) * parseInt(stock.volume);
+
+        } else if (isInt(parseInt(stock.price))) {
+
+            // Accumulated total - using int data
+            portfolioTotal += parseInt(stock.price) * parseInt(stock.volume);
+        }
+
 
         return (
         <Portfolio 
@@ -16,7 +43,9 @@ const PortfolioList = ({stocks}) => {
         )
     });
 
-    portfolioInfo.push("Current Portfolio: £ " + portfolioTotal);
+    portfolioTotal /= 100;
+
+    portfolioInfo.push("Current Portfolio: £ " + Math.round(portfolioTotal));
     return (
         <>
         {portfolioInfo}
